8d8844b3caef73bb4aba6417daf0a542a7a2f216
k19025136 <k19025136@kcl.ac.uk>
2021-02-08 17:39:02 +0300

    Add a way to read and write conf

--- board.cpp
+++ board.cpp
@@ -2,6 +2,8 @@
 using std::cout;
 #include <stdint.h>
 #include <string>
+#include <sstream>
+using std::stringstream;
 using std::string;
 #include <vector>
 using std::vector;
@@ -45,6 +47,12 @@ using std::vector;
  *      -> 4 bits per square of board
  *      -> the board struct stores a 2d array of bytes
  *      -> the config word is a 32 bits
+ *      - Config bits:
+ *          Bit 0:          Turn colour
+ *          Bit 1 - 4:      Castling Rights
+ *          Bit 5 - 8:      En passant
+ *          Bit 9 - 14:     Half move counter
+ *          Bit 15 - 31:    Full move counter
  *      
  *  Square:
  *      -> 8 bits per square
@@ -116,10 +124,43 @@ struct Board {
         
     }
 
+    void set_config(Int bit, Int value) {
+        Int mask = ~(1 << bit);
+        conf = (conf & mask) | (value << bit);
+    }
+
+    Byte get_config(Int bit) {
+        return (conf & (1 << bit)) != 0;
+    }
+
+    void set_enpassant(Int value) {
+        conf = (conf & ~(LO4 << 5)) | (value << 5);
+    }
+
+    Byte get_enpassant() {
+        return (conf & (LO4 << 5)) >> 5;
+    }
+
+    void set_halfmoves(Int value) {
+        conf = (conf & ~(63 << 9)) | (value << 9);
+    }
+
+    Int get_halfmoves() {
+        return (conf & (63 << 9)) >> 9;
+    }
+
+    void set_fullmoves(Int value) {
+        conf = (conf & ~(~0 << 15)) | (value << 15);
+    }
+
+    Int get_fullmoves() {
+        return (conf & (~0 << 15)) >> 15;
+    }
 };
 
 const Byte SHIFT_ROW = 16;
 const Byte SHIFT_COL = 1;
+
 
 inline Square mksq(int row, int col) {
     return (Square) ((row << 4) | col);
@@ -149,11 +190,11 @@ inline Square reset_row(Square s) {
     return s & LO4;
 }
 
-inline Byte row(Square s) {
+inline Byte get_row(Square s) {
     return s >> 4;
 }
 
-inline Byte col(Square s) {
+inline Byte get_col(Square s) {
     return s & LO4;
 }
 
@@ -165,7 +206,15 @@ inline bool val_col(Square s) {
     return !(s & 8);
 }
 
+inline Square stosq(string str) {
+    return mksq(str[1] - '1', str[0] - 'a');
+}
 
+inline string sqtos(Square sq) {
+    stringstream ss;
+    ss << (char) (get_col(sq) + 'a') << (char) (get_row(sq) + '1');
+    return ss.str();
+}
 
 inline bool is_white(Piece p) {
     return p & 8;
@@ -175,8 +224,8 @@ Board empty_board() {
     
     Board b;
     
-    for (Square s = mksq(0, 0) ; row(s) < 8; s = inc_row(s)) {
-        for ( ; col(s) < 8; s = inc_col(s)) {
+    for (Square s = mksq(0, 0) ; get_row(s) < 8; s = inc_row(s)) {
+        for ( ; get_col(s) < 8; s = inc_col(s)) {
             b.set(s, EMPTY);
         }
         s = reset_col(s);
@@ -316,12 +365,60 @@ void pr_indent(Board b, string indent) {
     }
 }
 
+
+void pr_config(Board b) {
+    cout << "Raw config: ";
+    for (int i = 31; i >= 0; --i) {
+        if (b.conf & (1 << i)) {
+            cout << "1";
+        } else {
+            cout << "0";
+        }
+
+        if (i == 0 || i == 1 || i == 5 || i == 9 || i == 15) {
+            cout << " ";
+        }
+    }
+    cout << "\n";
+    cout << "Turn: " << (b.get_config(0) ? "White" : "Black") << "\n";
+
+    cout << "Castle Rights: ";
+    if (b.get_config(1)) cout << "K ";
+    if (b.get_config(2)) cout << "Q ";
+    if (b.get_config(3)) cout << "k ";
+    if (b.get_config(4)) cout << "q ";
+    cout << "\n";
+
+    Int en = b.get_enpassant();
+    string enstr = sqtos(mksq(b.get_config(0) ? 5 : 2, b.get_enpassant() & 7));
+    cout << "Enpassant: " << (en < 8 ? "-" : enstr) << "\n";
+
+    cout << "Half moves: " << b.get_halfmoves() << "\n";
+    cout << "Full moves: " << b.get_fullmoves() << "\n";
+}
+
 void pr(Board b) {
     pr_indent(b, "");
 }
 
+inline string get_word(stringstream * ss) {
+    string word;
+    * ss >> word;
+    return word; 
+}
+
+inline int get_number(stringstream * ss) {
+    int num;
+    * ss >> num;
+    return num; 
+}
+
 /* work in progress - doesn't read the config word */
 Board fen_to_board(string fen) {
+    stringstream stream(fen);
+    string setup;
+
+    stream >> setup;
     
     Board b = empty_board();
     int i = 0;  // point in string
@@ -329,7 +426,7 @@ Board fen_to_board(string fen) {
     
     /* read in pieces first */
     unsigned row = 7, col = 0;
-    while ( i < fen.size() && (c = fen.at(i++)) != ' ') {
+    while ( i < setup.size() && (c = setup.at(i++)) != ' ') {
         
         Piece p;
         
@@ -360,28 +457,53 @@ Board fen_to_board(string fen) {
         }
         
     }
-    
+
+    string turn = get_word(& stream);
+    string castle = get_word(& stream);
+
+    b.set_config(0, turn == "w");
+
+    b.set_config(1, castle.find('K') != -1);
+    b.set_config(2, castle.find('Q') != -1);
+    b.set_config(3, castle.find('k') != -1);
+    b.set_config(4, castle.find('q') != -1);
+
+    string enpassant = get_word(& stream);
+    Byte en = enpassant == "-" ? 0 : get_col(stosq(enpassant) | 8);
+    b.set_enpassant(en);
+
+    int halfmoves = get_number(& stream);
+    b.set_halfmoves(halfmoves);
+
+    int fullmoves = get_number(& stream);
+    b.set_fullmoves(fullmoves);
+
     // if x,y != 7,7 return failure
     return b;
 }
 
 
-/*int main() {
 
-   
-    
+int main() {
+
     vector<string> test_fens = {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
         "rnbqkb1r/pp1p1ppp/5n2/2p5/4p3/1N4P1/PPPPPPBP/RNBQK2R b KQkq - 1 5",
         "r4rk1/ppB2pp1/4p1p1/2P3q1/4Pn2/P1N2n2/2B2PPP/2R2RK1 w - - 0 24",
-        "7k/1p6/p1p3p1/7p/1P2Q2P/P5P1/5r1K/5q2 w - - 4 47",
+        "7k/1p6/p1p3p1/7p/1P2Q2P/P5P1/5r1K/5q2 w - h6 4 47",
         "2r2rk1/1p2bppp/p2pbn2/q1N1p3/2P1P3/N3BP2/PP2B1PP/2RR2K1 w - - 0 17"
     };
     
     for (int i = 0; i < 5; ++i) {
-        pr(fen_to_board(test_fens[i]));
+        Board b = fen_to_board(test_fens[i]);
+        pr(b);
         cout << "\n";
+        cout << "FEN: " << test_fens[i] << "\n";
+        pr_config(b);
+        cout << "\n";
+
+        // cout << get_row(mksq("f3")) + 0;
     }
     
     return 0;
-}*/
+}
