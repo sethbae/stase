93d88883c7da75cf9a85fbb9de1ae6e0f3c77145
sethbae <sethwarren67@gmail.com>
2021-03-11 10:26:45 +0000

    moved gamestate bench to new package

--- board/board.h
+++ board/board.h
@@ -126,38 +126,6 @@ struct Move {
 
 };
 
-struct Gamestate
-{
-
-    // The enclosed board
-    Board board;
-
-    // Attack and occupation maps for each of the adversaries
-    Bitmap fattack;
-    Bitmap eattack;
-    Bitmap foccupy;
-    Bitmap eoccupy;
-
-    // Overall vacancy and occupation maps
-    Bitmap vacancy;
-    Bitmap occupancy;
-
-    // Locations of all pieces of a certain type (can be ANDed with occupation to isolate pieces of desired colour)
-    Bitmap kings;
-    Bitmap pawns;
-    Bitmap queens;
-    Bitmap knights;
-    Bitmap bishops;
-    Bitmap rooks;
-
-    Move last_move;
-    Piece last_capture;
-
-    Gamestate();
-    Gamestate(const Board &);
-    void recalculate_all();
-};
-
 
 /* helper functions for board and move */
 void make_move(Board &, Move);
