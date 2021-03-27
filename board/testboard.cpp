07f769576de83df037372012c07430cfb00aedb4
sethbae <sethwarren67@gmail.com>
2021-03-27 13:27:41 +0000

    added new tests for mutate

--- board/testboard.cpp
+++ board/testboard.cpp
@@ -13,6 +13,20 @@ using std::rand;
 
 /* correctness tests for all code in the board directory */
 
+void print_results(int succ, int fail, string test_name) {
+    cout << test_name;
+    for (int i = test_name.size(); i < 30; ++i)
+        cout << " ";
+        
+    cout << "Passed " << succ << "/" << succ + fail << " tests\n";
+    
+    if (fail == 1) {
+        cout << "*****ONE TEST FAILED*****\n";
+    } else if (fail > 1) {
+        cout << "*****SOME TESTS FAILED*****\n";
+    }
+}
+
 /* tests whether or not the pieces have correct types and colours associated with them */
 void piece_types() {
 
@@ -42,10 +56,8 @@ void piece_types() {
         }
     }
     
-    cout << "PIECE TYPES: Passed " << successful << "/" << successful + failed << " tests\n";
-    if (failed > 0) {
-        cout << "PIECE TYPES: " << failed << " TESTS FAILED\n";
-    }
+    
+    print_results(successful, failed, "PIECE_TYPES");
 
 }
 
@@ -73,10 +85,7 @@ void setget_square() {
         
     }
     
-    cout << "SET/GET SQUARE: Passed " << succ << "/" << succ+fail << " tests\n";
-    if (fail) {
-        cout << "SET/GET SQUARE: " << fail << " TESTS FAILED\n";
-    }
+    print_results(succ, fail, "SET/GET SQUARE");
     
 }
 
@@ -250,10 +259,7 @@ void square_navigation(bool print) {
             cout << "16\n";
     }
     
-    cout << "SQUARE NAVIGATION: Passed " << succ << "/" << succ+fail << " tests\n";
-    if (fail) {
-        cout << "SQUARE NAVIGATION: " << fail << " TESTS FAILED\n";
-    }
+        print_results(succ, fail, "SQUARE NAVIGATION");
 }
 
 void test_in_check() {
@@ -303,10 +309,11 @@ void test_in_check() {
         
     }
     
-    cout << "IN CHECK: Passed " << succ << "/" << test_fens.size() << " tests\n";
-    if (fail) {
-        cout << "IN CHECK: " << fail << " TESTS FAILED\n";
-    }
+        print_results(succ, fail, "IN CHECK");
+
+}
+
+void rw_fens() {
 
 }
 
@@ -323,6 +330,456 @@ void test_castling() {
     
 
     return;
+}
+
+void test_mutate_hard() {
+
+    Board b;
+    Move m;
+    int succ = 0, fail = 0;
+
+    // queen
+    
+    // Qd3-h7
+    b = fen_to_board("8/3q4/8/8/8/3Q4/8/8 w - - 0 1");
+    m = {mksq(3, 2), mksq(7, 6), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3q3Q/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // Qd3xd7
+    b = fen_to_board("8/3q4/8/8/8/3Q4/8/8 w - - 0 1");
+    m = {mksq(3, 2), mksq(3, 6), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3Q4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+
+    // rook    
+    
+    // Ra7-a2
+    b = fen_to_board("8/r2Q4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(0, 6), mksq(0, 1), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3Q4/8/8/8/8/r7/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // Ra7xd7
+    b = fen_to_board("8/r2Q4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(0, 6), mksq(3, 6), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3r4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bishop
+    
+    // Bg4-e2
+    b = fen_to_board("8/3r4/8/8/6B1/8/8/8 w - - 0 1");
+    m = {mksq(6, 3), mksq(4, 1), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3r4/8/8/8/8/4B3/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // Bg4xd7
+    b = fen_to_board("8/3r4/8/8/6B1/8/8/8 w - - 0 1");
+    m = {mksq(6, 3), mksq(3, 6), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3B4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // knight
+    
+    // Nf8-g6
+    b = fen_to_board("5n2/3B4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(5, 7), mksq(6, 5), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3B4/6n1/8/8/8/8/8 w - - 0 1")
+       ++succ;
+    else
+       ++fail;
+        
+    // Nf8xd7
+    b = fen_to_board("5n2/3B4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(5, 7), mksq(3, 6), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/3n4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // king (regular)
+     
+    // Ke1xe2
+    b = fen_to_board("8/8/8/q7/8/8/4bn2/4K3 w - - 0 1");
+    m = {mksq(4, 0), mksq(4, 1), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/8/8/q7/8/8/4Kn2/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // Ke1xf2
+    b = fen_to_board("8/8/8/q7/8/8/4bn2/4K3 w - - 0 1");
+    m = {mksq(4, 0), mksq(5, 1), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "8/8/8/q7/8/8/4bK2/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // king (castle)
+    
+    // ws
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 0), mksq(6, 0), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "r3k2r/8/8/8/8/8/8/R4RK1 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // wl
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 0), mksq(2, 0), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) ==  "r3k2r/8/8/8/8/8/8/2KR3R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bs
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 7), mksq(6, 7), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) ==  "r4rk1/8/8/8/8/8/8/R3K2R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bl
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 7), mksq(2, 7), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) ==  "2kr3r/8/8/8/8/8/8/R3K2R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // pawn (regular)
+    
+    // e5-e4
+    b = fen_to_board("r3k2r/8/8/4p3/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 4), mksq(4, 3), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) ==  "r3k2r/8/8/8/4p3/8/8/R3K2R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // pawn (e-p)
+    
+    // f5xe6
+    b = fen_to_board("r3k2r/8/8/4pP2/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(5, 4), mksq(4, 5), 0};
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "r3k2r/8/4P3/8/8/8/8/R3K2R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // pawn (promotion)
+    
+    // d8=Q
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(QUEEN);
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "3Q4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=R
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(ROOK);
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "3R4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=N
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(KNIGHT);
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "3N4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=B
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(BISHOP);
+    b.mutate_hard(m);
+    if (board_to_fen(b) == "3B4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+
+    
+    print_results(succ, fail, "MUTATE HARD");
+}
+
+void test_mutate() {
+
+    Board b;
+    Move m;
+    int succ = 0, fail = 0;
+
+    // queen
+    
+    // Qd3-h7
+    b = fen_to_board("8/3q4/8/8/8/3Q4/8/8 w - - 0 1");
+    m = {mksq(3, 2), mksq(7, 6), 0};
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3q3Q/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // Qd3xd7
+    b = fen_to_board("8/3q4/8/8/8/3Q4/8/8 w - - 0 1");
+    m = {mksq(3, 2), mksq(3, 6), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3Q4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+
+    // rook    
+    
+    // Ra7-a2
+    b = fen_to_board("8/r2Q4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(0, 6), mksq(0, 1), 0};
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3Q4/8/8/8/8/r7/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // Ra7xd7
+    b = fen_to_board("8/r2Q4/8/8/8/8/8/8 b - - 0 1");
+    m = {mksq(0, 6), mksq(3, 6), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3r4/8/8/8/8/8/8 b - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bishop
+    
+    // Bg4-e2
+    b = fen_to_board("8/3r4/8/8/6B1/8/8/8 w - - 0 1");
+    m = {mksq(6, 3), mksq(4, 1), 0};
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3r4/8/8/8/8/4B3/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // Bg4xd7
+    b = fen_to_board("8/3r4/8/8/6B1/8/8/8 w - - 0 1");
+    m = {mksq(6, 3), mksq(3, 6), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3B4/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // knight
+    
+    // Nf8-g6
+    b = fen_to_board("5n2/3B4/8/8/8/8/8/8 b - - 0 1");
+    m = {mksq(5, 7), mksq(6, 5), 0};
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3B4/6n1/8/8/8/8/8 b - - 0 1")
+       ++succ;
+    else
+       ++fail;
+        
+    // Nf8xd7
+    b = fen_to_board("5n2/3B4/8/8/8/8/8/8 b - - 0 1");
+    m = {mksq(5, 7), mksq(3, 6), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/3n4/8/8/8/8/8/8 b - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // king (regular)
+     
+    // Ke1xe2
+    b = fen_to_board("8/8/8/q7/8/8/4bn2/4K3 w - - 0 1");
+    m = {mksq(4, 0), mksq(4, 1), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/8/8/q7/8/8/4Kn2/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // Ke1xf2
+    b = fen_to_board("8/8/8/q7/8/8/4bn2/4K3 w - - 0 1");
+    m = {mksq(4, 0), mksq(5, 1), 0};
+    m.set_cap();
+    b.mutate(m);
+    if (board_to_fen(b) == "8/8/8/q7/8/8/4bK2/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // king (castle)
+    
+    // ws
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 0), mksq(6, 0), 0};
+    m.set_cas();
+    m.set_cas_short();
+    b.mutate(m);
+    if (board_to_fen(b) == "r3k2r/8/8/8/8/8/8/R4RK1 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // wl
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(4, 0), mksq(2, 0), 0};
+    m.set_cas();
+    m.unset_cas_short();
+    b.mutate(m);
+    if (board_to_fen(b) ==  "r3k2r/8/8/8/8/8/8/2KR3R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bs
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R b - - 0 1");
+    m = {mksq(4, 7), mksq(6, 7), 0};
+    m.set_cas();
+    m.set_cas_short();
+    b.mutate(m);
+    if (board_to_fen(b) ==  "r4rk1/8/8/8/8/8/8/R3K2R b - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // bl
+    b = fen_to_board("r3k2r/8/8/8/8/8/8/R3K2R b - - 0 1");
+    m = {mksq(4, 7), mksq(2, 7), 0};
+    m.set_cas();
+    m.unset_cas_short();
+    b.mutate(m);
+    if (board_to_fen(b) ==  "2kr3r/8/8/8/8/8/8/R3K2R b - - 0 1")
+        ++succ;
+    else
+        ++fail;
+    
+    // pawn (regular)
+    
+    // e5-e4
+    b = fen_to_board("r3k2r/8/8/4p3/8/8/8/R3K2R b - - 0 1");
+    m = {mksq(4, 4), mksq(4, 3), 0};
+    b.mutate(m);
+    if (board_to_fen(b) ==  "r3k2r/8/8/8/4p3/8/8/R3K2R b - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // pawn (e-p)
+    
+    // f5xe6
+    b = fen_to_board("r3k2r/8/8/4pP2/8/8/8/R3K2R w - - 0 1");
+    m = {mksq(5, 4), mksq(4, 5), 0};
+    m.set_cap();
+    m.set_ep();
+    b.mutate(m);
+    if (board_to_fen(b) == "r3k2r/8/4P3/8/8/8/8/R3K2R w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // pawn (promotion)
+    
+    // d8=Q
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(QUEEN);
+    m.set_prom();
+    b.mutate(m);
+    if (board_to_fen(b) == "3Q4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=R
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(ROOK);
+    m.set_prom();
+    b.mutate(m);
+    if (board_to_fen(b) == "3R4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=N
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(KNIGHT);
+    m.set_prom();
+    b.mutate(m);
+    if (board_to_fen(b) == "3N4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+        
+    // d8=B
+    b = fen_to_board("8/3P4/8/8/8/8/8/8 w - - 0 1");
+    m = {mksq(3, 6), mksq(3, 7), 0};
+    m.set_prom_piece(BISHOP);
+    m.set_prom();
+    b.mutate(m);
+    if (board_to_fen(b) == "3B4/8/8/8/8/8/8/8 w - - 0 1")
+        ++succ;
+    else
+        ++fail;
+
+    
+    print_results(succ, fail, "MUTATE");
+    
+}
+
+
+void test_legal_moves() {
+
 }
 
 void manual_gameplay() {
@@ -417,16 +874,19 @@ void manual_gameplay() {
 }*/
 
 int main(void) {
-    // piece_types();
-    // setget_square();
-    // square_navigation(false);
-    // test_in_check();
-    // gamestate_generation();
+    piece_types();
+    setget_square();
+    square_navigation(false);
+    test_in_check();
+    test_mutate_hard();
+    test_mutate();
+
+    //gamestate_generation();
     
     // Board b = starting_pos();
     // pr_bitmap(attack_map(b, WHITE));
     
-    manual_gameplay();
+    //manual_gameplay();
 
     return 0;
 }
