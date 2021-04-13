bcf9744ac35138ca860a58642e4c911833999c72
sethbae <sethwarren67@gmail.com>
2021-04-13 18:25:09 +0100

    example search and results

--- scratch.cpp
+++ scratch.cpp
@@ -10,12 +10,31 @@ using std::vector;
 int main(void) {
 
     Gamestate gs;
-    gs.board = starting_pos();
+    //gs.board = starting_pos();
+    gs.board = fen_to_board("4k3/4q3/8/8/1B6/8/8/4K3 w - - 0 1");
     
-    vector<Move> vec;
+    vector<SearchNode*> nodes;
     
-    depth_limited_search(gs, 4);
+    nodes = depth_limited_search(gs, 5);
+    
+    readable_printout(nodes);
+    
+    /*for (SearchNode* node : nodes) {
         
+        pr_board_conf(node->gs->board);   
+        
+    }
+    
+    Board b = fen_to_board("8/4Bk2/8/8/8/8/8/4K3 w - - 0 1");
+    vector<Move> moves;
+    
+    legal_moves(b, moves);
+    
+    for (Move m : moves) {
+        cout << movetosan(b, m) << " ";
+    }
+    cout << "\n";
+    */
     return 0;
 
 }
