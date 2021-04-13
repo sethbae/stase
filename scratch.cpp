603da46b7cdd54cbc5e8b0d0772a58bd890a3b52
sethbae <sethwarren67@gmail.com>
2021-04-13 17:08:08 +0100

    example of generating depth limited search

--- scratch.cpp
+++ scratch.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using std::cout;
+#include <vector>
+using std::vector;
 
 #include "board/board.h"
 #include "game/game.h"
+#include "search/search.h"
 
 int main(void) {
 
     Gamestate gs;
     gs.board = starting_pos();
     
-    cout << heur(gs) << "\n";
+    vector<Move> vec;
     
+    depth_limited_search(gs, 4);
+        
     return 0;
 
 }
