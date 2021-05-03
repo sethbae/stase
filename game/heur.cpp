b0ed4a412a033f241015130531a69482d7d66437
sethbae <sethwarren67@gmail.com>
2021-05-03 16:04:22 +0100

    improved open line measuring

--- game/heur.cpp
+++ game/heur.cpp
@@ -204,7 +204,10 @@ float piece_activity_alpha(const Board & b) {
         }
     }
     
-    float w_proportion = ((float)w_control / (float)(b_control + w_control));
+    float w_proportion = 0.0;
+    
+    if (w_control + b_control > 0)
+        w_proportion = ((float)w_control / (float)(b_control + w_control));
     
     return -1.0 + 2*w_proportion;
     
@@ -230,7 +233,10 @@ float piece_activity_beta(const Board & b) {
         }
     }
     
-    float w_proportion = ((float)w_control / (float)(b_control + w_control));
+    float w_proportion = 0.0;
+    
+    if (w_control + b_control > 0)
+        w_proportion = ((float)w_control / (float)(b_control + w_control));
     
     return -1.0 + 2*w_proportion;
     
@@ -256,7 +262,10 @@ float piece_activity_gamma(const Board & b) {
         }
     }
     
-    float w_proportion = ((float)w_control / (float)(b_control + w_control));
+    float w_proportion = 0.0;
+    
+    if (w_control + b_control > 0)
+        w_proportion = ((float)w_control / (float)(b_control + w_control));
     
     return -1.0 + 2*w_proportion;
     
@@ -281,10 +290,10 @@ float centre_control(const Board & b) {
     
 }
 
-// counts the number of pieces on the given rank, file or diagonal which can move in that
-// direction. Ignores other pieces (notably pawns). It increments for white, and decrements
-// for black.
-int line_control(const Board & b, Square s, StepFunc *step, MoveType dir) {
+// Open files: return the number of squares controlled on the given file (which is presumed
+// to contain no pawns - tho  this is not checked). 
+// Essentially this is just 7 squares for whoever controls the file.
+int open_line_walk(const Board & b, Square s, StepFunc *step, MoveType dir) {
     
     int control = 0;
     
@@ -297,11 +306,74 @@ int line_control(const Board & b, Square s, StepFunc *step, MoveType dir) {
         (*step)(s);
     }
     
-    return control;
+    // White conrols the file
+    if (control > 0)
+        return 7;
+        
+    // Black controls the file
+    if (control < 0)
+        return -7;
+        
+    // nobody does
+    return 0;
+}
+
+// returns an integer representing the difference in controlled squares on a half open file.
+// That is, it counts through the line in  two chunks: before pawn, after pawn. Each half gets 
+// its length as score provided that one side has a piece on it (that moves correctly). If a 
+// half has pieces of both sides on it, that scores zero. White scores count upwards, black 
+// scores count downwards, and the net result is returned.
+// See open_line_control
+int half_line_walk(const Board & b, Square s, StepFunc *step, MoveType dir) {
+    
+    int score = 0;      // the overall score which we'll return
+    unsigned squares_before_pawn = 0;
+    int control = 0;    // who controls this run of squares
+    int signum = 0;     // -1 -> black does, 0 -> nobody does, 1 -> white does
+    
+    // walk up to the pawn
+    while (val(s) && (b.get(s) != PAWN)) {
+        ++squares_before_pawn;  // counting squares
+        
+        Piece p = b.get(s);
+        if (can_move(p, dir)) { // and control
+            control += (colour(p) == WHITE) ? 1 : -1;
+        }
+        (*step)(s);
+    }
+    
+    // calculate contribution to score
+    if (control < 0)
+        --signum;
+    else if (control > 0)
+        ++signum;
+    score += signum * squares_before_pawn;
+    
+    // and now check who controls the other section
+    (*step)(s);
+    while (val(s)) {
+        Piece p = b.get(s);
+        if (can_move(p, dir)) {
+            control += (colour(p) == WHITE) ? 1 : -1;
+        }
+        (*step)(s);
+    }
+    
+    // and calculate contribution of second half
+    signum = 0;
+    if (control < 0)
+        --signum;
+    else if (control > 0)
+        ++signum;
+    // we already know the length
+    score += signum * (7 - squares_before_pawn);
+    
+    return score;
 }
 
 // counts the number of pawns reachable from the given square according to the step func,
 // ie the number which are on a certain rank or file
+// See open_line_control
 unsigned count_pawns(const Board & b, Square s, StepFunc *step) {
     
     unsigned sum = 0;
@@ -320,71 +392,123 @@ unsigned count_pawns(const Board & b, Square s, StepFunc *step) {
 
 // who controls the open lines and diagonals? This checks ranks, files and major diagonals
 float open_line_control(const Board & b) {
- 
-    // TODO: this only counts lines which have no pawns on. For something like the 
-    // seventh rank, this is rarely appropriate - some more sophisticated measure is needed.
+
+    /*
+    
+    This tries to measure who controls the position's open lines. It measures this as follows:
+    
+        -files are open (no pawns), half open (1 pawn) or closed (2 pawns)
+        -files can be controlled by one side or no sides (but not both)
+        -different files get different weights (see look up tables)
+        
+    It examines all ranks, files, and the six longest diagonals, scoring thus:
+    
+        -open: 7 points to whoever controls it
+        -half: for each half, 1 point per square to the side who controls it
+        -closed: no points
+        
+    Points are then scaled by the weights and the whole thing is normalised over 300, a number
+    with no real justification but which seems to work ok.
+    
+    NB the half open files are scored per file, not per half - so black's points on that file
+    are subtracted from white's. That's slightly different to how open files work, which lends
+    more weight to the open files - which I think is alright.
+    
+    */
 
     int score = 0;
+    Square sq;
+    unsigned num_pawns;
  
     unsigned FILE_VALUES[] = {
      // a  b  c  d  e  f  g  h
-        1, 2, 3, 3, 3, 3, 2, 1
+        10, 12, 15, 15, 15, 15, 12, 10
     };
     
     unsigned RANK_VALUES[] = {
-  // y =0  1  2  3  4  5  6  7  
-        3, 2, 1, 1, 1, 1, 2, 3
+  // y =0  1  2  3  4  5  6  7
+        15, 15, 10, 10, 10, 10, 15, 15
     };
     
     unsigned DIAG_VALUES[] = {
-    // a2  a1 b1
-        1, 3, 1,    // positive 3 diagonals
-    // b8  a8 a7    // negative 3 diagonals
-        1, 3, 1
+    //  a2  a1  b1
+        10, 20, 10,   // positive 3 diagonals
+    //  b8  a8  a7 
+        10, 20, 10    // negative 3 diagonals
     };
  
     // files 
     for (unsigned x = 0; x < 8; ++x) {
-        Square sq = mksq(x, 0);
-        if (count_pawns(b, sq, inc_y) == 0) {
-            score += line_control(b, sq, inc_y, ORTHO) * FILE_VALUES[x];
-        }
+        sq = mksq(x, 0);
+        num_pawns = count_pawns(b, sq, inc_y);
+        if (num_pawns == 0)
+            score += open_line_walk(b, sq, inc_y, ORTHO) * FILE_VALUES[x];
+        else if (num_pawns == 1)
+            score += half_line_walk(b, sq, inc_y, ORTHO) * FILE_VALUES[x];
     }
+    
+    int file_score = score;
+    cout << "File score: " << score << "\n";
     
     // ranks
     for (unsigned y = 0; y < 8; ++y) {
-        Square sq = mksq(0, y);
-        if (count_pawns(b, sq, inc_x) == 0) {
-            score += line_control(b, sq, inc_x, ORTHO) * RANK_VALUES[y];
-        }
+        sq = mksq(0, y);
+        num_pawns = count_pawns(b, sq, inc_x);
+        if (num_pawns == 0)
+            score += open_line_walk(b, sq, inc_x, ORTHO) * RANK_VALUES[y];
+        else if (num_pawns == 1)
+            score += half_line_walk(b, sq, inc_x, ORTHO) * RANK_VALUES[y];
     }
     
+    int rank_score = score;
+    cout << "Rank score: " << score - file_score << "\n";
+    
     // diagonals
-    Square sq = stosq("a2");
-    if (count_pawns(b, sq, diag_ur) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[0];
-        
+    sq = stosq("a2");
+    num_pawns = count_pawns(b, sq, diag_ur);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[0];
+    else if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[0];
+    
     sq = stosq("a1");
-    if (count_pawns(b, sq, diag_ur) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[1];
+    num_pawns = count_pawns(b, sq, diag_ur);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[1];
+    else if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[1];
     
     sq = stosq("b1");
-    if (count_pawns(b, sq, diag_ur) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[2];
+    num_pawns = count_pawns(b, sq, diag_ur);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[2];
+    else if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_ur, DIAG) * DIAG_VALUES[2];
         
     sq = stosq("b8");
-    if (count_pawns(b, sq, diag_dr) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[3];
+    num_pawns = count_pawns(b, sq, diag_dr);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[3];
+    if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[3];
         
     sq = stosq("a8");
-    if (count_pawns(b, sq, diag_dr) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[4];
+    num_pawns = count_pawns(b, sq, diag_dr);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[4];
+    if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[4];
     
     sq = stosq("a7");
-    if (count_pawns(b, sq, diag_dr) == 0)
-        score += line_control(b, sq, diag_ur, DIAG) * DIAG_VALUES[5];
+    num_pawns = count_pawns(b, sq, diag_dr);
+    if (num_pawns == 0)
+        score += open_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[5];
+    if (num_pawns == 1)
+        score += half_line_walk(b, sq, diag_dr, DIAG) * DIAG_VALUES[5];
         
-    return ((float)score) / 20.0;
+    cout << "Diag score: " << score - rank_score << "\n";
+        
+    return ((float)score) / 300.0;
         
 }
 
--- scratch.cpp
+++ scratch.cpp
@@ -47,8 +47,9 @@ int main(void) {
     */
     
     
-    Board b = fen_to_board("5r2/3b2kp/6p1/8/5B2/2P5/2r2PBP/R5K1 w - - 1 35");
-
+    // Board b = fen_to_board("5r2/3b2kp/6p1/8/5B2/2P5/2r2PBP/R5K1 w - - 1 35");
+    // Board b = fen_to_board("rn3rk1/pp4pp/3bq3/3p1p2/3Pn3/1Q3N2/PP2BPPP/R1B1R1K1 w - - 2 15");
+    Board b = fen_to_board("r1bq1rk1/1p1p1ppp/p1nbpn2/8/4P3/2NPBNP1/PP3PBP/2RQ1RK1 w q - 0 1");
 /*
     pr_board(b);
 
