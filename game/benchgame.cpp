452f0b7710e7840d69461d9b132c7ef380bbc654
sethbae <sethwarren67@gmail.com>
2021-06-06 12:27:57 +0100

    add ehur benchmark

--- game/benchgame.cpp
+++ game/benchgame.cpp
@@ -24,6 +24,7 @@ using std::ios;
 using std::time;
 
 #include "game.h"
+#include "../puzzle/puzzle.h"
 
 void gamestate_recalculation() {
     Board b = fen_to_board("r1br2k1/1p3ppp/p1nqpb2/3nN3/3P1P2/1B2B3/PPN3PP/R2Q1RK1 w Qq - 0 1");
@@ -113,9 +114,46 @@ void calculating_check_with_gamestate() {
     delete [] states;
 }
 
+void heuristic_evaluation() {
+    
+    vector<string> vec;
+    read_all_fens(vec);
+    
+    // initialise all boards
+    Gamestate * states = new Gamestate[vec.size()];
+    for (int i = 0; i < vec.size(); ++i) {
+        states[i].board = fen_to_board(vec[i]);
+    }
+    
+    // start timer
+    auto start = high_resolution_clock::now();
+    
+    double scores = 0.0;
+    
+    for (int i = 0; i < vec.size(); ++i) {
+        scores += heur(states[i]);
+    }
+    
+    /* end benchmark and return */
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+    
+    double millis = duration.count() / 1000.0;
+    double per_board = duration.count() / (double) vec.size();
+    
+    cout << "Time taken for " << vec.size() << " boards was " << millis << " milliseconds ";
+    cout << "(" << per_board << " microseconds per board)\n";  
+    cout << "\t(Sum: " << scores << ")\n";
+    cout << "\n";
+    
+    delete [] states;
+    
+}
+
 int main(void) {
 
-    gamestate_recalculation();
+    heuristic_evaluation();
 
 }
 
