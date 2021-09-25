65389d4417b6174b59f103260f393626d5dcbbae
sethbae <sethwarren67@gmail.com>
2021-09-25 14:55:54 +0100

    improve benchmarks #1

--- src/bench/bench.h
+++ src/bench/bench.h
@@ -1,6 +1,31 @@
 #ifndef STASE_BENCH_H
 #define STASE_BENCH_H
 
+#include <chrono>
+
+enum BenchUnit {
+    NANOS,
+    MICROS,
+    MILLIS,
+    SECONDS
+};
+
+inline std::string unit_name(BenchUnit u) {
+    switch (u) {
+        case NANOS:
+            return "ns";
+        case MICROS:
+            return "us";
+        case MILLIS:
+            return "ms";
+        case SECONDS:
+            return "s";
+        default:
+            return "unknown unit";
+    }
+}
+
+
 int bench_board();
 
 int bench_game();
