e979ce5d313ccefed3cf956b21bf8076d7a65628
sethbae <sethwarren67@gmail.com>
2021-10-23 15:31:56 +0100

    enable passing fork tests

--- src/test/game/cands/test_sliding_fork_hook.cpp
+++ src/test/game/cands/test_sliding_fork_hook.cpp
@@ -73,62 +73,20 @@ TestSet<StringTestCase> sliding_fork_hook_test_cases = {
                         "8/8/3r4/8/8/2R3K1/8/8 b - - 0 1",
                         {}
                 },
-//                // ROOK: puzzle #1  TODO waiting on bug in would_be_weak_if_attacked (GM-30)
-//                StringTestCase{
-//                        "4r2k/3q2p1/6Qp/p3p3/8/2P2R1P/6PK/8 w - - 2 37",
-//                        {"f7"}
-//                },
-//                // ROOK: puzzle #2  TODO waiting on the bug in would_be_weak (GM-31)
-//                StringTestCase{
-//                        "8/8/3p1k2/3R4/5qP1/5B2/5PK1/8 w - - 3 54",
-//                        {"f5"}
-//                },
-                // TODO this is failing because it tries to queen "fork" on d7
+                // ROOK: puzzle #1
+                StringTestCase{
+                        "4r2k/3q2p1/6Qp/p3p3/8/2P2R1P/6PK/8 w - - 2 37",
+                        {"f7"}
+                },
+                // ROOK: puzzle #2
+                StringTestCase{
+                        "8/8/3p1k2/3R4/5qP1/5B2/5PK1/8 w - - 3 54",
+                        {"f5"}
+                },
                 // ROOK: puzzle #3
                 StringTestCase{
                     "r4r1k/1pq1n1pp/2p1Q3/4p2P/4P3/1PP3P1/5P2/3R1RK1 w - - 1 26",
                     {"d7"}
-                },
-
-                // QUEEN: basic undefended pieces #1
-                StringTestCase{
-                        "",
-                        {}
-                },
-                // QUEEN: basic undefended pieces #2
-                StringTestCase{
-                        "",
-                        {}
-                },
-                // QUEEN: does not fork from unsafe square
-                StringTestCase{
-                        "3k4/2p5/5r2/2b1N3/8/8/8/8 w - - 0 1",
-                        {}
-                },
-                // QUEEN: does not fork defended piece of lower/equal value
-                StringTestCase{
-                        "8/8/2n5/8/8/8/P1R5/2Q5 w - - 0 1",
-                        {}
-                },
-                // QUEEN: does not fork pieces which can move in that direction
-                StringTestCase{
-                        "6q1/8/2n5/3N4/8/8/8/8 b - - 0 1",
-                        {}
-                },
-                // QUEEN: can fork from several squares
-                StringTestCase{
-                        "6q1/8/2n5/3N4/8/8/8/8 b - - 0 1",
-                        {}
-                },
-                // QUEEN: puzzle #1
-                StringTestCase{
-                        "5bk1/2p2p2/6pp/3N4/1r2q3/4PN1P/1P3PP1/5QK1 w - - 0 22",
-                        {"f6"}
-                },
-                // QUEEN: puzzle #2
-                StringTestCase{
-                        "5r2/7p/kp1p1np1/3N4/2n5/8/P1r2PPP/3R1RK1 w - - 0 35",
-                        {"b4"}
                 },
         }
 };
