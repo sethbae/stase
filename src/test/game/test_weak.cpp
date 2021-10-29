778dfbb72ba03a8b833cba706f0b51a63c72a5c7
sethbae <sethwarren67@gmail.com>
2021-10-29 18:28:23 +0100

    1st test

--- src/test/game/test_weak.cpp
+++ src/test/game/test_weak.cpp
@@ -1,12 +1,13 @@
 #include "../test.h"
 #include "game.h"
+#include <string.h>
 
 struct WeakTestCase {
 
     const std::string fen;
-    std::vector<Square> squares;
-    std::vector<Move> moves;
-    std::vector<Ptype> colours;
+    std::vector<std::string> squares;
+    std::vector<std::string> moves;
+    std::vector<std::string> colours;
     std::vector<bool> expected_results;
 
 };
@@ -14,14 +15,23 @@ struct WeakTestCase {
 struct UnsafeTestCase {
 
     const std::string fen;
-    std::vector<Square> squares;
-    std::vector<Move> moves;
+    std::vector<std::string> squares;
+    std::vector<std::string> moves;
     std::vector<bool> expected_results;
 
 };
 
 const TestSet<WeakTestCase> weak_test_cases = {
-
+    "game-piece-weak-square",
+    {
+        WeakTestCase{
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            {"e1", "e8", "a1", "h8", "e4", "e4"},
+            {"none", "none", "none", "none", "none", "none"},
+            {"white", "black", "white", "black", "white", "black"},
+            {false, false, false, false, false, false}
+        }
+    }
 };
 
 const TestSet<UnsafeTestCase> unsafe_test_cases = {
@@ -34,15 +44,29 @@ bool evaluate_weak_test_case(const WeakTestCase * tc) {
 
     for (int i = 0; i < tc->squares.size(); ++i) {
 
-        Square s = tc->squares[i];
-        Move m = tc->moves[i];
+        Square s = stosq(tc->squares[i]);
+
+        Move m;
+        if (strcmp(tc->moves[i].c_str(), "none") == 0) {
+            m = MOVE_SENTINEL;
+        } else {
+            m = Move{
+                    stosq(tc->moves[i].substr(0,2)),
+                    stosq(tc->moves[i].substr(2,4))
+            };
+        }
+
+        Ptype colour =
+            strcmp(tc->colours[i].c_str(), "white") == 0
+                ? WHITE
+                : BLACK;
 
         if (is_sentinel(m)) {
-            if (is_weak_square(gs, s, tc->colours[i]) != tc->expected_results[i]) {
+            if (is_weak_square(gs, s, colour) != tc->expected_results[i]) {
                 return false;
             }
         } else {
-            if (would_be_weak_after(gs, s, tc->colours[i], m) != tc->expected_results[i]) {
+            if (would_be_weak_after(gs, s, colour, m) != tc->expected_results[i]) {
                 return false;
             }
         }
@@ -57,8 +81,17 @@ bool evaluate_unsafe_test_case(const UnsafeTestCase * tc) {
 
     for (int i = 0; i < tc->squares.size(); ++i) {
 
-        Square s = tc->squares[i];
-        Move m = tc->moves[i];
+        Square s = stosq(tc->squares[i]);
+
+        Move m;
+        if (strcmp(tc->moves[i].c_str(), "none") == 0) {
+            m = MOVE_SENTINEL;
+        } else {
+            m = Move{
+                stosq(tc->moves[i].substr(0,2)),
+                stosq(tc->moves[i].substr(2,4))
+            };
+        }
 
         if (is_sentinel(m)) {
             if (is_unsafe_piece(gs, s) != tc->expected_results[i]) {
