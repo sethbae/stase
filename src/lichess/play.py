719dfbf0c9af6f0c3b5034ec43d06848accbaf21
sethbae <sethwarren67@gmail.com>
2021-12-14 14:27:29 +0000

    use subprocess not os

--- src/lichess/play.py
+++ src/lichess/play.py
@@ -1,8 +1,10 @@
 import os
+import subprocess
+
 from lichess.client import stream_game_events, make_move
 
-GAME_FILE_DIR = "~/Documents/stase_lichess"
-STASE_EXEC_DIR = "~/CLionProjects/stase2"
+GAME_FILE_DIR = "/home/seth/Documents/stase_lichess"
+STASE_EXEC_DIR = "/home/seth/CLionProjects/stase2"
 
 
 def play_game(token: str, game_id: str, as_white: bool):
@@ -27,14 +29,13 @@ def play_game(token: str, game_id: str, as_white: bool):
 
 def _get_move(game_id: str, moves_played: str) -> str:
 
-    with open(f"{GAME_FILE_DIR}/{game_id}.game") as file:
+    with open(f"{GAME_FILE_DIR}/{game_id}.game", "w") as file:
         file.write(moves_played)
 
-    os.system(f"cd {STASE_EXEC_DIR}")
+    exec_stase_command: str = f"./stase -g {game_id}"
+    subprocess.Popen(exec_stase_command.split(), cwd=STASE_EXEC_DIR).wait()
 
-    os.system(f"./stase -g {game_id}")
-
-    with open(f"{GAME_FILE_DIR}/{game_id}.game") as file:
+    with open(f"{GAME_FILE_DIR}/{game_id}.game", "r") as file:
         move: str = file.readline().strip()
 
     return move
