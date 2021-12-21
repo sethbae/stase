ba7751c41b2c45c11880ad176ec4996c2ead9dc8
sethbae <sethwarren67@gmail.com>
2021-12-21 11:06:19 +0000

    add min/max utils

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,6 +6,18 @@
 // converts floating point to two decimal places
 std::string dp_2(double);
 
+template <typename T>
+constexpr T max(const T a, const T b) {
+    if (a > b) { return a; }
+    return b;
+}
+
+template <typename T>
+constexpr T min(const T a, const T b) {
+    if (a < b) { return a; }
+    return b;
+}
+
 Move uci2move(const std::string &);
 std::string move2uci(const Move);
 
