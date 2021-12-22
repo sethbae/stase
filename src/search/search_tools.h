83bb0d22427e9a667eeff293ca0796ba9ba0a106
sethbae <sethwarren67@gmail.com>
2021-12-22 15:24:11 +0000

    move file writer into header

--- src/search/search_tools.h
+++ src/search/search_tools.h
@@ -9,5 +9,6 @@ std::vector<SearchNode *> retrieve_best_line(SearchNode *);
 void check_abort();
 
 void print_line(std::vector<SearchNode *> & line);
+void record_tree_in_file(const std::string & filename, SearchNode * root);
 
 #endif //STASE_SEARCH_TOOLS_H
