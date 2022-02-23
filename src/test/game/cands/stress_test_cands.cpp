6e7357db2d693e4ba24547d24deac50fbe7d2b63
sethbae <sethwarren67@gmail.com>
2022-02-23 10:41:23 +0000

    fix stress tests

--- src/test/game/cands/stress_test_cands.cpp
+++ src/test/game/cands/stress_test_cands.cpp
@@ -24,10 +24,10 @@ bool stress_test_individual_hook(std::vector<Gamestate> & states, const Hook * h
         discover_feature_frames(gs, h);
 
         FeatureFrame ff;
-        for (int j = 0; !is_sentinel((ff = gs.frames[h->id][j]).centre); ++j) {
+        for (int j = 0; j < MAX_FRAMES && !is_sentinel((ff = gs.frames[h->id][j]).centre); ++j) {
 
             if (!val(ff.centre)) {
-                cout << "\n[" << j << "] FAILED: Received " << sqtos(ff.centre) << "\n";
+                cout << "\n[" << i << "] FAILED: Received " << sqtos(ff.centre) << "\n";
                 cout << board_to_fen(gs.board) << "\n";
                 pr_board(gs.board);
                 return false;
