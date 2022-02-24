53afb32d196da10ba44b01536ea84bf63130450a
sethbae <sethwarren67@gmail.com>
2022-02-24 13:02:39 +0000

    fix tests #2

--- src/test/game/cands/test_queen_fork_hook.cpp
+++ src/test/game/cands/test_queen_fork_hook.cpp
@@ -11,34 +11,28 @@ TestSet<ForkTestCase> queen_fork_hook_test_cases = {
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
             {}
         },
-        // basic undefended pieces #1
+        // does not find normal forks #1
         ForkTestCase{
             "8/6b1/3Q4/8/8/8/6n1/8 w - - 0 1",
+            {}
+        },
+        // basic undefended pieces #1
+        ForkTestCase{
+            "8/3r4/7Q/8/8/8/6r1/8 w - - 0 1",
             {
-                ForkFrame{"g2", "g7", "b7", "d6"}
+                ForkFrame{"d7", "g2", "c6", "h6"},
+                ForkFrame{"d7", "g2", "h3", "h6"}
             }
         },
-            // basic undefended pieces #2
+        // no forks reachable
         ForkTestCase{
-                "8/3r4/7Q/8/8/8/6r1/8 w - - 0 1",
-                {
-                        ForkFrame{"d7", "g2", "c6", "h6"},
-                        ForkFrame{"d7", "g2", "h3", "h6"}
-                }
+            "8/8/3q4/8/4Q3/8/8/R5K1 b - - 0 1",
+            {}
         },
-            // undefended pieces #3
+        // does not find normal forks #2
         ForkTestCase{
-                "8/8/3q4/8/4Q3/8/8/R5K1 b - - 0 1",
-                {
-                        ForkFrame{"a1", "g1", "g7", "d6"}
-                }
-        },
-            // basic undefended pieces #4
-        ForkTestCase{
-                "8/1q6/8/8/8/8/N5B1/8 b - - 0 1",
-                {
-                        ForkFrame{"a2", "g2", "g8", "b7"}
-                }
+            "8/1q6/8/8/8/8/N5B1/8 b - - 0 1",
+            {}
         },
         // none available
         ForkTestCase{
@@ -55,16 +49,14 @@ TestSet<ForkTestCase> queen_fork_hook_test_cases = {
             "8/8/8/8/8/3q4/1R4B1/8 b - - 0 1",
             {}
         },
-        //  gives multiple choices #1
+        //  basic undefended pieces #2
         ForkTestCase{
             "8/1p3p2/3Q4/8/8/8/8/8 w - - 0 1",
             {
-                ForkFrame{"b7", "f7", "b3", "d6"},
                 ForkFrame{"b7", "f7", "d5", "d6"},
-                ForkFrame{"b7", "f7", "f3", "d6"}
             }
         },
-        //  gives multiple choices #2
+        //  gives multiple choices #1
         ForkTestCase{
             "8/8/3q4/8/8/8/1P3P2/8 b - - 0 1",
             {
@@ -97,33 +89,22 @@ TestSet<ForkTestCase> queen_fork_hook_test_cases = {
         ForkTestCase{
             "r2q4/5ppk/p2p3p/1p1P4/2p1rNQ1/8/PPP3PP/5RK1 w - - 0 23",
             {
-                ForkFrame{"a2", "c2", "a4", "d8"},
-                ForkFrame{"g1", "b2", "d4", "d8"},
-                ForkFrame{"f7", "e4", "f5", "g4"},
-                ForkFrame{"e4", "g7", "h7", "g4"}
+                ForkFrame{"f7", "e4", "f5", "g4"}
             }
         },
         // puzzle #2 (it finds an extra fork, on b3)
         ForkTestCase{
             "r1b1k2r/ppq2ppp/5n2/3p4/1b1P4/2NBB3/PP3PPP/R2QK2R w KQkq - 3 11",
             {
-                ForkFrame{"g2", "e1", "h1", "c7"},
-                ForkFrame{"h2", "h1", "g2", "c7"},
-                ForkFrame{"d5", "b4", "b5", "d1"},
                 ForkFrame{"d5", "b4", "b3", "d1"},
                 ForkFrame{"e8", "b4", "a4", "d1"},
-                ForkFrame{"g7", "d5", "g5", "d1"},
-                ForkFrame{"g7", "e8", "h8", "d1"}
             }
         },
         // puzzle #3 (the correct solution is not actually a fork, but this tests the logic regardless)
         ForkTestCase{
             "r5qr/p1p1p2p/2Qp2p1/5k2/5n2/5P2/PP3K1P/2R3NR w - - 0 21",
             {
-                ForkFrame{"f5", "c7", "a5", "c6"},
                 ForkFrame{"f5", "e7", "e4", "c6"},
-                ForkFrame{"f2", "b2", "d4", "g8"},
-                ForkFrame{"h1", "f2", "g2", "g8"},
             }
         }
     }
