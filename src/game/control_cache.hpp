161f2a9b0a694500b6968839972b7fb0ddb501ce
sethbae <sethwarren67@gmail.com>
2022-03-04 14:36:05 +0000

    refactor control_cache #3

--- src/game/control_cache.hpp
+++ src/game/control_cache.hpp
@@ -9,9 +9,12 @@
  */
 struct ControlCache {
 
-    uint64_t squares = 0;
+    uint64_t status_present = 0;
     SquareControlStatus status_cache[64];
-    uint8_t control_count_cache[64];
+
+    uint64_t count_present = 0;
+    int8_t control_count_cache[64];
+
     const Gamestate * gs;
 
     /**
@@ -19,10 +22,17 @@ struct ControlCache {
      * computes and adds its status before returning it.
      */
     inline SquareControlStatus get_control_status(const Square s) {
-        if (contains(s)) { return get_control(s); }
+        if (contains_status(s)) { return get_status(s); }
         SquareControlStatus status = evaluate_square_control(*gs, s);
-        put_control(s, status);
+        put_status(s, status);
         return status;
+    }
+
+    inline int8_t get_control_count(const Square s) {
+        if (contains_count(s)) { return get_count(s); }
+        int count = control_count(*gs, s);
+        put_count(s, count);
+        return count;
     }
 
     /**
@@ -36,14 +46,17 @@ struct ControlCache {
         Square invalidated[64];
         find_invalidated_squares(b, &invalidated[0], m);
 
-        squares = o.squares;
+        status_present = o.status_present;
+        count_present = o.count_present;
         for (int i = 0; i < 64; ++i) {
             status_cache[i] = o.status_cache[i];
             control_count_cache[i] = o.control_count_cache[i];
         }
 
         for (int i = 0; i < 64 && !is_sentinel(invalidated[i]); ++i) {
-            squares &= ~(1l << index(invalidated[i]));
+            const uint64_t MASK = ~(1l << index(invalidated[i]));
+            status_present &= MASK;
+            count_present &= MASK;
         }
     }
 
@@ -52,18 +65,30 @@ private:
         return s.x + (8 * s.y);
     }
 
-    inline bool contains(const Square s) {
-        return squares & (1l << index(s));
+    inline bool contains_status(const Square s) {
+        return status_present & (1l << index(s));
     }
 
-    inline SquareControlStatus get_control(const Square s) {
-        int ind = s.x + (8*s.y);
-        return status_cache[ind];
+    inline bool contains_count(const Square s) {
+        return count_present & (1l << index(s));
     }
 
-    inline void put_control(const Square s, SquareControlStatus status) {
+    inline SquareControlStatus get_status(const Square s) {
+        return status_cache[index(s)];
+    }
+
+    inline int8_t get_count(const Square s) {
+        return control_count_cache[index(s)];
+    }
+
+    inline void put_status(const Square s, SquareControlStatus status) {
         status_cache[index(s)] = status;
-        squares |= (1l << index(s));
+        status_present |= (1l << index(s));
+    }
+
+    inline void put_count(const Square s, int8_t count) {
+        control_count_cache[index(s)] = count;
+        count_present |= (1l << index(s));
     }
 };
 
