f6220bdf5449c990b6bccab02af15e2463c2ce54
sethbae <sethwarren67@gmail.com>
2022-03-15 16:22:08 +0000

    pool challenges

--- src/lichess/main.py
+++ src/lichess/main.py
@@ -1,5 +1,9 @@
+import multiprocessing
 import subprocess
 import sys
+import time
+from typing import List, Any, Dict
+
 from client import (
     respond_to_challenge,
     stream_incoming_events,
@@ -66,11 +70,77 @@ def play_single_games():
             print(f"Ignoring event of type: {event['type']}")
 
 
+class ChallengePool:
+
+    def __init__(self, size: int, access_token: str):
+        self.size: int = size
+        self.access_token: str = access_token
+        self.procs: List = [None for _ in range(self.size)]
+        self.proc_names: Dict[Any, str] = {}
+        self.q: List = []
+
+    def games_in_play(self) -> int:
+        return len([p for p in self.procs if p is not None])
+
+    def add_challenge(self, event) -> None:
+        self.q.append(event)
+
+    def refresh_challenges(self) -> None:
+
+        # get rid of dead processes
+        for i in range(len(self.procs)):
+            proc = self.procs[i]
+            if proc is not None and not proc.is_alive():
+                print(f"Game {self.proc_names[proc]} has finished")
+                self.proc_names[proc] = ""
+                self.procs[i] = None
+
+        # kick off as many challenges as possible
+        while len(self.q) > 0 and self.games_in_play() < self.size:
+            challenge = self.q.pop(0)
+            respond_to_challenge(self.access_token, challenge["challenge"]["id"])
+            time.sleep(0.5)
+            i: int = self.procs.index(None)
+            self.procs[i] =\
+                multiprocessing.Process(target=play_game, args=(self.access_token, challenge["challenge"]["id"]))
+            self.procs[i].start()
+            self.proc_names[self.procs[i]] = challenge["challenge"]["id"]
+
+
+def play_games(max_concurrent: int) -> None:
+    """
+    Accepts all challenges. Plays up to :param max_concurrent: games at once, queueing
+    other challenges and accepting in order received.
+    """
+
+    token: str = read_access_token()
+    cpool: ChallengePool = ChallengePool(max_concurrent, token)
+
+    print("Waiting for incoming challenges.")
+
+    for event in stream_incoming_events(token):
+        print(f"Received event {event}")
+        if "error" in event:
+            print(f"Received error: {event}")
+        elif event["type"] == "challenge":
+            cpool.add_challenge(event)
+        elif event["type"] == "gameStart":
+            # handled inside play_game
+            pass
+        elif event["type"] == "gameFinish":
+            # no action required
+            pass
+        else:
+            print(f"Ignoring event of type: {event['type']}")
+
+        cpool.refresh_challenges()
+
+
 def main():
     print("Building engine...", end="")
     rebuild_stase()
     print("done!")
-    play_single_games()
+    play_games(5)
 
 
 if __name__ == "__main__":
