ab1d14ba9fd0cd22d268fee188d1c3c9f559edb3
sethbae <sethwarren67@gmail.com>
2022-03-21 14:34:45 +0000

    util for ptr to string

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,9 @@
 #ifndef STASE_UTILS_H
 #define STASE_UTILS_H
 
+#include <sstream>
+#include <string>
+
 #include "board.h"
 
 // converts floating point to two decimal places
@@ -22,5 +25,16 @@ Move uci2move(const std::string &);
 std::string move2uci(const Move);
 
 void print_stack_trace(int sig);
+
+/**
+ * Returns a string representation of the memory address pointed to by the given pointer.
+ */
+template<typename T>
+inline std::string to_string(const T * ptr) {
+    const void * p = static_cast<const void*>(ptr);
+    std::stringstream ss;
+    ss << p;
+    return ss.str();
+}
 
 #endif //STASE_UTILS_H
