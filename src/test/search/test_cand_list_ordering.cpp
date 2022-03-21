8601f1851f8b65c97a5873cc2deca0e08d67a210
sethbae <sethwarren67@gmail.com>
2022-03-21 11:33:30 +0000

    add first test

---
+++ src/test/search/test_cand_list_ordering.cpp
@@ -0,0 +1,97 @@
+#include "search.h"
+#include "../../search/observers/observers.hpp"
+#include "../../game/gamestate.hpp"
+#include "../test.h"
+
+class CandListOrderingObserver : public Observer {
+
+public:
+    bool passed = true;
+    int null_count = 0;
+    std::vector<std::string> diagnostics;
+
+    inline void open_event(const SearchNode * node, const SearchEvent ev, const CandList * cand_list) {
+
+        if (!node || !node->cand_set || !cand_list) {
+            ++null_count;
+            return;
+        }
+
+        if (ev == DEEPEN) {
+            switch (*cand_list) {
+                case CRITICAL:
+                    break;
+                case MEDIAL:
+                    if (node->cand_set->critical.size() > 0) {
+                        fail_test(node, cand_list);
+                    }
+                    break;
+                case FINAL:
+                    if (node->cand_set->critical.size() > 0
+                        || node->cand_set->medial.size() > 0) {
+                        fail_test(node, cand_list);
+                    }
+                    break;
+                case LEGAL:
+                    if (node->cand_set->critical.size() > 0
+                        || node->cand_set->medial.size() > 0
+                        || node->cand_set->final.size() > 0) {
+                        fail_test(node, cand_list);
+                    }
+                    break;
+            }
+        }
+    }
+
+    inline void close_event(const SearchNode * node, const SearchEvent, const CandList *) {}
+
+    inline bool passed_test() {
+        return passed;
+    }
+
+private:
+
+    inline void fail_test(const SearchNode * node, const CandList * cand_list) {
+        diagnostics.push_back(
+            board_to_fen(node->gs->board)
+            + ": Called with " + name(*cand_list) + " with CandSet sizes ("
+            + std::to_string(node->cand_set->critical.size()) + ","
+            + std::to_string(node->cand_set->medial.size()) + ","
+            + std::to_string(node->cand_set->final.size()) + ","
+            + std::to_string(node->cand_set->legal.size()) + ")\n"
+        );
+        passed = false;
+    }
+
+};
+
+const TestSet<std::string> search_cand_list_test_set{
+    "search-cand-list",
+    {
+        "r2qkbnr/pp1bpppp/3p4/2p5/4P3/5N2/PPPP1PPP/RNBQ1RK1 b kq - 1 5",
+        "3r1rk1/p1q2ppp/2bbpn2/1pp5/8/1P1P1NP1/PBPN1P1P/R2Q1RK1 w - - 0 14",
+        "8/8/R3pkp1/7p/3P4/8/pr6/6K1 b - - 1 55"
+    }
+};
+
+bool evaluate_cand_list_test_case(const std::string * fen) {
+
+    std::cout << "Evaluating test case\n";
+
+    CandListOrderingObserver obs;
+    run_with_node_limit(*fen, 25000, obs);
+
+    std::cout << "Nulls: " << obs.null_count << "\n";
+
+    if (!obs.passed_test()) {
+        for (const std::string & diag : obs.diagnostics) {
+            std::cout << diag;
+        }
+        return false;
+    }
+    return true;
+}
+
+bool test_cand_list_ordering() {
+    return evaluate_test_set(&search_cand_list_test_set, &evaluate_cand_list_test_case);
+}
