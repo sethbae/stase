0c843ffb88c5e6d67e9aa588877f8ee4934dffb5
sethbae <sethwarren67@gmail.com>
2022-03-27 17:26:42 +0100

    more tests (failing)

--- src/test/search/test_past_blunders.cpp
+++ src/test/search/test_past_blunders.cpp
@@ -16,7 +16,43 @@ const TestSet<SearchLineTestCase> past_blunders_test_set{
                     "e4 c5 Ke2"
                 }
             }
-        }
+        },
+        SearchLineTestCase{
+            // hung queen against Timo
+            "r1b1kb1r/pp1p1ppp/4pn2/2q5/1n2P3/3Q1N2/PPPB1PPP/RN2KB1R w KQkq - 2 8",
+            {
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "Qe3 Nxc2 Kd1 Nxe3"
+                },
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "Qe3 Nxc2 Ke2 Nxe3"
+                }
+            }
+        },
+        SearchLineTestCase{
+            // bizarre underpromotion
+            "1r4k1/3Pq1p1/4p2p/4Pp2/5P2/3QN1P1/5KB1/4N3 w - - 3 48",
+            {
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "d8=Q Rxd8"
+                },
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "d8=R Rxd8"
+                },
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "d8=B Rxd8"
+                },
+                SearchLineRequirement{
+                    MUST_APPEAR,
+                    "d8=N Rxd8"
+                }
+            }
+        },
     }
 };
 
