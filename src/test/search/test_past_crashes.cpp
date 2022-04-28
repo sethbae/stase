cf22d873ab9afff2ac2ab98c122069d1876fd65b
sethbae <sethwarren67@gmail.com>
2022-04-28 17:53:55 +0100

    re-enable tests (yay)

--- src/test/search/test_past_crashes.cpp
+++ src/test/search/test_past_crashes.cpp
@@ -6,7 +6,7 @@ const TestSet<SearchLineTestCase> past_crashes_test_set{
     {
         SearchLineTestCase{"2r5/p5Qp/3b4/2n4k/8/1P4P1/PB5P/2R2RK1 b - - 0 30", {}},
         SearchLineTestCase{"8/8/8/6p1/5k2/8/5K2/8 w - - 0 52", {}},
-//        SearchLineTestCase{"8/6p1/8/K7/3k4/1r6/8/1b6 w - - 20 74", {}},
+        SearchLineTestCase{"8/6p1/8/K7/3k4/1r6/8/1b6 w - - 20 74", {}},
         SearchLineTestCase{"r4rk1/q4ppp/p3p3/1bbp4/p2Q4/P3PNP1/1PP2PBP/2KR3R w - - 3 19", {}},
     }
 };
