b1ff726164317e68349f75ac30f6ab98e39efc7b
sethbae <sethwarren67@gmail.com>
2022-05-16 20:24:49 +0100

    bugfix?

--- src/search/engine.cpp
+++ src/search/engine.cpp
@@ -22,7 +22,7 @@ void * Engine::start(void * args) {
     auto start = std::chrono::high_resolution_clock::now();
     greedy_search(search_args->root, search_args->cycles, *search_args->o);
     auto stop = std::chrono::high_resolution_clock::now();
-    double secs = duration_cast<std::chrono::milliseconds>(stop - start).count() / 1000;
+    double secs = duration_cast<std::chrono::microseconds>(stop - start).count() / 1000000;
 
     *search_args->nodes_out = subtree_size(search_args->root);
     *search_args->move_out = current_best_move(search_args->root);
