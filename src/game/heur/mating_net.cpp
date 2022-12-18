fe5a7f414708800ba89d1f1bc8385e944d721b37
sethbae <sethwarren67@gmail.com>
2022-12-18 12:20:50 +0000

    basic implementation

--- src/game/heur/mating_net.cpp
+++ src/game/heur/mating_net.cpp
@@ -1,6 +1,13 @@
 #include "heur.h"
-#include "heur_util.h"
+#include "../king_net.hpp"
+#include "../gamestate.hpp"
 
-float __metrics::__mating_net(const Gamestate &) {
-    return 0.0f;
+float score(const KingNet * king_net) {
+    return ((float) (8 - king_net->flight_squares())) / 8.0f;
+}
+
+float __metrics::__mating_net(const Gamestate & gs) {
+    return gs.board.get_white()
+        ? 1 * score(gs.b_king_net)
+        : -1 * score(gs.w_king_net);
 }
