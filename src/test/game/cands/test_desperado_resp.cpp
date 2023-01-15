bbdf3f214ec45b37302afd19893afc106cfc201e
sethbae <sethwarren67@gmail.com>
2023-01-15 15:33:25 +0000

    fix whitespace

--- src/test/game/cands/test_desperado_resp.cpp
+++ src/test/game/cands/test_desperado_resp.cpp
@@ -1,39 +1,39 @@
 #include "../../test.h"
 
 const TestSet<ResponderTestCase> desperado_test_cases = {
-        "game-cands-desperado-piece",
-        {
-            // does nothing if no captures are available
-            ResponderTestCase {
-                "8/8/8/4N3/8/8/8/8 w - - 0 1",
-                {FeatureFrame{stosq("e5"), SQUARE_SENTINEL, 0, 0}},
-                {}
-            },
-            // does not capture piece of own colour
-            ResponderTestCase {
-                "8/8/8/3p4/2b5/8/8/8 b - - 0 1",
-                {FeatureFrame{stosq("c4"), SQUARE_SENTINEL, 0, 0}},
-                {}
-            },
-            // captures most valuable piece
-            ResponderTestCase {
-                "3r4/8/8/1q1R1b2/8/8/3n4/8 w - - 0 1",
-                {FeatureFrame{stosq("d5"), SQUARE_SENTINEL, 0, 0}},
-                {"d5b5"}
-            },
-            // captures defended piece of lower value if only option available
-            ResponderTestCase {
-                "1b6/2P5/1P6/8/8/8/8/8 b - - 0 1",
-                {FeatureFrame{stosq("b8"), SQUARE_SENTINEL, 0, 0}},
-                {"b8c7"}
-            },
-            // puzzle #1
-            ResponderTestCase {
-                "4rrk1/6p1/pQ5p/1pb1P2n/8/3n1PB1/PP2N1PP/R4RK1 w - - 2 22",
-                {FeatureFrame{stosq("b6"), SQUARE_SENTINEL, 0, 0}},
-                {"b6c5"}
-            },
-        }
+    "game-cands-desperado-piece",
+    {
+        // does nothing if no captures are available
+        ResponderTestCase {
+            "8/8/8/4N3/8/8/8/8 w - - 0 1",
+            {FeatureFrame{stosq("e5"), SQUARE_SENTINEL, 0, 0}},
+            {}
+        },
+        // does not capture piece of own colour
+        ResponderTestCase {
+            "8/8/8/3p4/2b5/8/8/8 b - - 0 1",
+            {FeatureFrame{stosq("c4"), SQUARE_SENTINEL, 0, 0}},
+            {}
+        },
+        // captures most valuable piece
+        ResponderTestCase {
+            "3r4/8/8/1q1R1b2/8/8/3n4/8 w - - 0 1",
+            {FeatureFrame{stosq("d5"), SQUARE_SENTINEL, 0, 0}},
+            {"d5b5"}
+        },
+        // captures defended piece of lower value if only option available
+        ResponderTestCase {
+            "1b6/2P5/1P6/8/8/8/8/8 b - - 0 1",
+            {FeatureFrame{stosq("b8"), SQUARE_SENTINEL, 0, 0}},
+            {"b8c7"}
+        },
+        // puzzle #1
+        ResponderTestCase {
+            "4rrk1/6p1/pQ5p/1pb1P2n/8/3n1PB1/PP2N1PP/R4RK1 w - - 2 22",
+            {FeatureFrame{stosq("b6"), SQUARE_SENTINEL, 0, 0}},
+            {"b6c5"}
+        },
+    }
 };
 
 bool evaluate_desperado_test_case(const ResponderTestCase * tc) {
