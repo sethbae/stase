773ef2568970e209ad56d8417758151f4c2a39bd
sethbae <sethwarren67@gmail.com>
2023-01-15 15:08:50 +0000

    comments

--- src/game/cands/position.cpp
+++ src/game/cands/position.cpp
@@ -2,6 +2,14 @@
 #include "hook.hpp"
 #include "../gamestate.hpp"
 
+/**
+ * Writes feature frames for pieces which could feature in a positional move, defined as anything other than a pawn or
+ * king. Format of the FeatureFrame:
+ * centre: the square of the piece
+ * secondary: sentinel
+ * conf1: unused
+ * conf2: unused
+ */
 bool positional_move_hook(Gamestate & gs, const Square s) {
     if (is_not_pk(gs.board.get(s)) && !gs.is_kpinned_piece(s)) {
         return gs.add_frame(positional_hook.id, FeatureFrame{s, SQUARE_SENTINEL, 0, 0});
@@ -9,6 +17,13 @@ bool positional_move_hook(Gamestate & gs, const Square s) {
     return true;
 }
 
+/**
+ * Writes feature frames for each king, so that positional endgame moves can be played. Format of the FeatureFrame:
+ * centre: the square of the king
+ * secondary: sentinel
+ * conf1: unused
+ * conf2: unused
+ */
 bool king_positional_hook(Gamestate & gs, const Square s) {
     if (type(gs.board.get(s)) == KING) {
         return gs.add_frame(king_position_hook.id, FeatureFrame{s, SQUARE_SENTINEL, 0, 0});
