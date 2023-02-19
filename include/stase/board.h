8861541da1cc81272aa99083bda3502c4b0b7875
sethbae <sethwarren67@gmail.com>
2023-02-19 09:13:50 +0000

    check game status

--- include/stase/board.h
+++ include/stase/board.h
@@ -18,6 +18,15 @@
  */
 struct Board;
 
+enum GameStatus {
+    ONGOING,
+    WHITE_WON,
+    BLACK_WON,
+    STALEMATE,
+    DRAW_BY_THREEFOLD,
+    DRAW_BY_FIFTY_MOVES
+};
+
 inline std::string starting_fen() {
     return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 }
@@ -27,11 +36,41 @@ inline std::string starting_fen() {
  * as well as checking whether you're in check or not.
  */
 namespace game_rules {
+
+    /** @return true if the player to move is in check */
     bool in_check(const Board &);
+
+    /** @return true if the player of the given colour is in check */
     bool in_check(const Board &, Colour);
+
+    /**
+     * Fill the given ptr_vec with the moves which could be made by the piece on the given square,
+     * disregarding whether or not they would be moving into check.
+     */
     void piecemoves_ignore_check(const Board &, Square, ptr_vec<Move> &);
+
+    /**
+     * Fill the given vector with all legal moves available in the given position.
+     */
     void legal_moves(const Board &, std::vector<Move> &);
+
+    /**
+     * Return a vector containing all legal moves available in the given position.
+     */
     std::vector<Move> legal_moves(const Board &);
+
+    /**
+     * @return the GameStatus implied by the current position. This is not able to account for
+     * threefold repetition.
+     */
+    GameStatus check_status(const Board &);
+    GameStatus check_status(const std::string & fen);
+
+    /**
+     * @return the GameStatus implied by the current position, given the preceding positions.
+     */
+    GameStatus check_status(const std::vector<Board> &);
+    GameStatus check_status(const std::vector<std::string> & fens);
 }
 
 /**
---
+++ src/board/game_status.cpp
@@ -0,0 +1,56 @@
+#include "board.h"
+#include "board.hpp"
+
+GameStatus game_rules::check_status(const Board & b) {
+
+    bool in_check = game_rules::in_check(b);
+    std::vector<Move> legal_moves = game_rules::legal_moves(b);
+
+    if (legal_moves.empty()) {
+        if (!in_check) {
+            return STALEMATE;
+        }
+        return b.get_white() ? BLACK_WON : WHITE_WON;
+    }
+
+    if (b.get_halfmoves() >= 50) {
+        return DRAW_BY_FIFTY_MOVES;
+    }
+
+    return ONGOING;
+}
+
+GameStatus game_rules::check_status(const std::string & fen) {
+    return game_rules::check_status(board_utils::fen_to_board(fen));
+}
+
+GameStatus game_rules::check_status(const std::vector<Board> & boards) {
+
+    const Board & current_pos =boards.back();
+    GameStatus status = game_rules::check_status(current_pos);
+
+    if (status != ONGOING) {
+        return status;
+    }
+
+    int count = 0;
+
+    // we now search the board history
+    for (int i = (int)boards.size() - 1; i >= 0; --i) {
+        const Board & b = boards[i];
+        if (b.equivalent(current_pos)) {
+            if (++count == 3) {
+                return DRAW_BY_THREEFOLD;
+            }
+        }
+    }
+    return ONGOING;
+}
+
+GameStatus game_rules::check_status(const std::vector<std::string> & fens) {
+    std::vector<Board> boards(fens.size());
+    for (const std::string & fen : fens) {
+        boards.push_back(board_utils::fen_to_board(fen));
+    }
+    return game_rules::check_status(boards);
+}
